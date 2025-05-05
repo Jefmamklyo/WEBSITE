fac46e343e37902db7f74df013bbb06cb0372abf
Jefmamklyo <129337859+Jefmamklyo@users.noreply.github.com>
2025-05-05 15:02:20 +1000

    Add files via upload

---
+++ Actual Assignment/Actual Assignment/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Actual_Assignment
+{
+    public partial class Form1 : Form
+    {
+        Form2 f2 = new Form2();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            f2.Owner = this;
+            f2.Location = this.Location;
+            f2.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
