b39a1b17872794d9b032d15605432555416746f9
Jefmamklyo <129337859+Jefmamklyo@users.noreply.github.com>
2025-05-08 14:53:15 +1000

    Add files via upload

---
+++ Actual Assignment/Actual Assignment/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Actual_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnProc = new System.Windows.Forms.Button();
+            this.lblDisp = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 370);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 387);
+            this.textBox1.MaxLength = 4;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(276, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnProc
+            // 
+            this.btnProc.Location = new System.Drawing.Point(300, 574);
+            this.btnProc.Name = "btnProc";
+            this.btnProc.Size = new System.Drawing.Size(75, 23);
+            this.btnProc.TabIndex = 2;
+            this.btnProc.Text = "Proceed";
+            this.btnProc.UseVisualStyleBackColor = true;
+            this.btnProc.Click += new System.EventHandler(this.btnProc_Click);
+            // 
+            // lblDisp
+            // 
+            this.lblDisp.AutoSize = true;
+            this.lblDisp.Location = new System.Drawing.Point(177, 434);
+            this.lblDisp.Name = "lblDisp";
+            this.lblDisp.Size = new System.Drawing.Size(0, 13);
+            this.lblDisp.TabIndex = 3;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 659);
+            this.Controls.Add(this.lblDisp);
+            this.Controls.Add(this.btnProc);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnProc;
+        private System.Windows.Forms.Label lblDisp;
+    }
+}
